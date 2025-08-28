642ad7125b5bf548449cfbdfd7b24217c2b6c20e
Rodri <136265952+RReis00@users.noreply.github.com>
2025-08-28 20:04:26 +0100

    added portfolio

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="../src/assets/CV_Rodrigo_R.docx"
+                href="/Rodrigo_Reis_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
