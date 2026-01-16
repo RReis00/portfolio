65ed88702cf9be2acb7628de1481a2845cbb6174
Rodri <136265952+RReis00@users.noreply.github.com>
2026-01-16 14:14:07 +0000

    new cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
 
               <a
                 target="_blank"
-                href="/Rodrigo_Reis_CV.pdf"
+                href="/Rodrigo_Reis_CVE.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
